58d4770ffbf1b73b87e312957b8a059e79fa1f74
tokechan <fleatoke@gmail.com>
2025-04-24 00:12:35 +0900

    Add:drizzle ORM

---
+++ db/schema.ts
@@ -0,0 +1,14 @@
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const files = sqliteTable("files", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => randomUUID()),
+  fileName: text("fileName").notNull(),
+  filePath: text("filePath").notNull(),
+  contentType: text("contentType").notNull(),
+  expiresAt: text("expiresAt").notNull(),
+  createdAt: text("createdAt")
+    .notNull()
+    .$defaultFn(() => new Date().toISOString()),
+});
